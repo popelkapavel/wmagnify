74549ae289fe83b8ad4572520aa9158724e99d43
unknown <popelkapavel@seznam.cz>
2022-12-03 19:09:05 +0100

    1

---
+++ src/mc.bat
@@ -0,0 +1,2 @@
+@rem gcc -Wall -fomit-frame-pointer -O3 -o wmagnify.exe wmagnifr.o wmagnify.c -lgdi32 -Wno-misleading-indentation && wmagnify -t 25x
+gcc -Wall -fomit-frame-pointer -O3 -o wmagnify.exe wmagnifr.o wmagnify.c -lgdi32  && wmagnify -t 25x
